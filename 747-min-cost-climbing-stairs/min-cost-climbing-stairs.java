4559c05246029258deec57b6f9883fc07df14b6f
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2025-12-04 23:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (9.12%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int helper(int[] cost,int n,int[] dp){
+    // MMEOIZATION
+    // public int helper(int[] cost,int n,int[] dp){
 
-        if(n==0 || n==1){
-            return cost[n];
+    //     if(n==0 || n==1){
+    //         return cost[n];
 
-        }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=cost[n]+Math.min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+    //     }
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=cost[n]+Math.min(helper(cost,n-1,dp),helper(cost,n-2,dp));
        
-    }
+    // }
+
+    //  TABULATION
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
         int dp[]=new int[n];
-        Arrays.fill(dp,-1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
         
-        return Math.min(helper(cost,n-1,dp) , helper(cost,n-2,dp));
+        return Math.min(dp[n-2],dp[n-1]);
         
     }
 
