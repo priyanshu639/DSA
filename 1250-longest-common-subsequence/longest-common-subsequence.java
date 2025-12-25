8e36f7ea226807df16df4c41e18b4d5022fc3470
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2025-12-25 21:32:43 +0530

    Time: 23 ms (42.14%) | Memory: 54.3 MB (9.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,28 +1,31 @@
 class Solution {
-    
     public int longestCommonSubsequence(String text1, String text2) {
-        int m=text1.length();
-        int n=text2.length();
-
-        int dp[][]=new int[m+1][n+1];
-        // TABULATION APPROACH
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                if(i==0 || j==0 ) dp[i][j]=0;
-                else {
-                    if(text1.charAt(i-1)==text2.charAt(j-1)){
-                        dp[i][j] = 1 + dp[i-1][j-1];
-                    }
-                    else{
-                        dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-                }
-
+        int n=text1.length();
+        int m=text2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j]= 1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                
             }
-            
         }
 
-        return dp[m][n];
+        // YAHA PE HUM PRINT KARENGE LCS KO
+        StringBuilder s = new StringBuilder("");
+        int i=n;int j=m;
+        while(i>0 && j>0){
+            if(text1.charAt(i-1)==text2.charAt(j-1)) s.append(text1.charAt(i-1));
+            if(dp[i-1][j]>dp[i][j-1]) i--;
+            else j--;
+            }
+        
+        s.reverse();
+        System.out.println(s);
+        return dp[n][m];
+
         
     }
 }
