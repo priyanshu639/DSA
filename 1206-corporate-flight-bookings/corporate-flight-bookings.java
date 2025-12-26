eb45f3e593b8d1e63c6020ed867aabcc229440a2
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2025-12-27 05:23:30 +0530

    Time: 3 ms (98.62%) | Memory: 85.2 MB (7.93%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,14 +1,19 @@
 class Solution {
-    // BRUTE FORCE APPPROACH TC IS O(N*M)
+    // OPTIMAL APPPROACH TC IS O(N+M)
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int ans[]=new int[n];
         for(int i=0;i<bookings.length;i++){
             int first=bookings[i][0];
             int last=bookings[i][1];
             int seats=bookings[i][2];
-            for(int j=first-1;j<=last-1;j++){
-                ans[j]+=seats;
-            }
+            ans[first-1]+=seats;
+            if(last<n)ans[last]-=seats;
+           
+        }
+
+        for(int j=1;j<ans.length;j++){
+            ans[j]+=ans[j-1];
+
         }
         return ans;
     }
