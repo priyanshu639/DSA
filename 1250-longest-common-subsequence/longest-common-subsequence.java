57b34af89396ceaab784c230d6d7a48aa97d8450
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-01-01 02:29:18 +0530

    Time: 22 ms (49.14%) | Memory: 53.9 MB (66.74%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -17,8 +17,13 @@ class Solution {
         StringBuilder s = new StringBuilder("");
         int i=n;int j=m;
         while(i>0 && j>0){
-            if(text1.charAt(i-1)==text2.charAt(j-1)) s.append(text1.charAt(i-1));
-            if(dp[i-1][j]>dp[i][j-1]) i--;
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                s.append(text1.charAt(i-1));
+                i--;
+                j--;
+
+            } 
+            else if(dp[i-1][j]>dp[i][j-1]) i--;
             else j--;
             }
         
