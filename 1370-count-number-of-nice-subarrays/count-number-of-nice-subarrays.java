bbab5124243c19f1945775af81746b3653428e87
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-01-01 00:47:01 +0530

    Time: 55 ms (5.68%) | Memory: 61.7 MB (5.11%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,26 +1,28 @@
 class Solution {
-    // method 1
     public int numberOfSubarrays(int[] nums, int k) {
-        int a=0;int b=0;int k2=0;int count=0;int n=nums.length;int i=0;int j=0;
-        while(i<n && nums[i]%2==0) i++;
-        while(j<n && k2<k) if(nums[j++]%2!=0) k2++;       
-        if(k2<k)  return 0;
-        j--;       
-        b=j+1;
-        while(b<n && nums[b]%2==0) b++;
-        b--;
-        // upar ke 4 variable set hogaye abb sliding window tecjhnique lagao
-        // sliding window
-        while(b<n){
-            count+=(i-a+1)*(b-j+1);
-            a=i+1;
-            i++;
-            while(i<n && nums[i]%2==0)i++;
-            j=b+1;
-            b=j+1;
-            while(b<n && nums[b]%2==0)b++;
-            b--; 
+        // Method 2
+        
+        // this is prefixsum and hashmap approach 
+        int n=nums.length, count=0;
+        for(int i=0;i<n;i++)nums[i] %= 2;
+        for(int i=1;i<n;i++)nums[i]+=nums[i-1];
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],i);
+            }
+            int a=0;
+            if(map.containsKey(nums[i]-k)){
+                a=map.get(nums[i]-k);
+            }
+            int b=0;
+             if(map.containsKey(nums[i]-k+1)){
+                b=map.get(nums[i]-k+1);
+            }
+            if(nums[i]==k)count+=b-a+1;
+            if(nums[i]>k) count+=b-a;
         }
+
         return count;
     }
 }
