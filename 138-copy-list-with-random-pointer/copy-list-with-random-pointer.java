daa1988aaac1bf8d4b5e4dbc06449791aa95afb7
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-01-14 11:26:19 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (88.53%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,38 +14,63 @@ class Node {
 */
 
 class Solution {
+    public Node deepcopy(Node head){
+     
+       Node head2=new Node(head.val);
+       Node t1=head.next;
+       Node t2=head2;
+       while(t1!=null){
+        Node node=new Node(t1.val);
+        t2.next=node;
+        t2=t2.next;
+        t1=t1.next;
+       }
+       return  head2;
+    }
+
+    public void connectAlternatively(Node head,Node head2){
+         Node dummy=new Node(-1);
+        Node t=dummy;
+        Node head1=head;
+        
+        while(head1!=null && head2!=null){
+            t.next=head1;
+            head1=head1.next;
+            t=t.next;
+            t.next=head2;
+            head2=head2.next;
+            t=t.next;
+
+        }
+    }
     public Node copyRandomList(Node head) {
-        if(head==null) return null;
-        Node head2=new Node(head.val);
-        Node temp2=head2;
-        Node temp=head.next;
-        
-        while(temp!=null){
-            Node dup=new Node(temp.val);
-            temp2.next=dup;
-            temp2=dup;
-            temp=temp.next;
+        if(head==null) return head;
+        Node head2=deepcopy(head);//deep copy done 
+        // STEP -2
+        // join these two linkedlist alternatively,using the concept of merge two LL
+        connectAlternatively(head,head2);
+       
 
+        // step 3 
+        // Assign random pointer
+        Node t1=head;
+        Node t2=head2;
+        while(t1!=null){
+            t2=t1.next;
+            if(t1.random!=null) t2.random=t1.random.next;
+            t1=t1.next.next;
         }
-        
-        HashMap<Node,Node> map=new HashMap<>();
-        temp=head;
-        temp2=head2;
-        while(temp!=null){
-            map.put(temp,temp2);
-            temp=temp.next;
-            temp2=temp2.next;
-        }
-        for(Node ele:map.keySet()){
-            Node duplic=map.get(ele);
-            if(ele.random!=null){
-                duplic.random=map.get(ele.random);
-
-            }
+        // STEP - 4 split the LL
+        t1=head;
+        t2=head2;
+        while(t1!=null){
+            t1.next=t2.next;
+            t1=t1.next;
+            if(t1==null) break;
+            t2.next=t1. next;
+            t2=t2.next;
         }
         return head2;
-        
-        
         
     }
 }
