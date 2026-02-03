4acd0a212ae88b4146164296e41b484141798f8d
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-02-03 23:45:05 +0530

    Time: 0 ms (100.00%) | Memory: 110.9 MB (59.35%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,37 +1,16 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        // TC AND SC =O(n);
-        // Aprroach1=>using indegree 
-        
-        HashMap<Integer,Integer>map=new HashMap<>();
-        // <node ,degree>
-        for(int i=0;i<edges.length;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            if(map.containsKey(u)){
-                map.put(u,map.get(u)+1);
 
-            }
-            else{
-                map.put(u,1);
-            }
-            if(map.containsKey(v)){
-                map.put(v,map.get(v)+1);
+       
+        int a = edges[0][0];
+        int b = edges[0][1];
 
-            }
-            else{
-                map.put(v,1);
-            }
-            
-
+        int c = edges[1][0];
+        int d = edges[1][1];
+        if (a == c || a == d) {
+            return a;
+        } else {
+            return b;
         }
-        int n=map.size();
-        for(int ele:map.keySet()){
-            if(map.get(ele)==n-1){
-                return ele;
-            }
-        }
-        return -1;
-        
     }
 }
