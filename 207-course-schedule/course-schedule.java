cd9043a9af62f17a4d1a16f0bbd9ad9c39481982
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-02-06 18:05:28 +0530

    Time: 5 ms (78.05%) | Memory: 47.1 MB (76.21%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,40 +1,47 @@
 class Solution {
+    static boolean ans;
+    public void dfs(int i,List<List<Integer>>ll,boolean vis[],boolean path[]){
+        vis[i]=true;
+        path[i]=true;
+        for(int ele:ll.get(i)){
+            if(path[ele]==true){
+                ans=false;
+                return;
+            }
+            if(vis[ele]==false){
+                dfs(ele,ll,vis,path);
+            }
+        }
+        path[i]=false;
+
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ans=true;
         List<List<Integer>>ll=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             ll.add(new ArrayList<>());
         }
-        int indegree[]=new int[numCourses];
+      
         for(int i=0;i<prerequisites.length;i++){
             int a=prerequisites[i][0];
             int b=prerequisites[i][1];
             ll.get(b).add(a);
-            indegree[a]++;
+        
 
         }
-        // apply kahn algorithm
+    //apply dfs
         
-        Queue<Integer>q=new LinkedList<>();
-        ArrayList<Integer>ans=new ArrayList<>();
-        for(int i=0;i<indegree.length;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        }
-        
-        while(q.size()>0){
-            int front=q.remove();
-            ans.add(front);
-            for(int ele:ll.get(front)){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.add(ele);
-                }
+        boolean vis[]=new boolean[numCourses];
+        boolean path[]=new boolean[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==false){
+                 dfs(i,ll,vis,path);
                 
             }
+            
         }
-        if(ans.size()==numCourses) return true;
-        else return false;
+        return ans;
+       
         
     }
 }
