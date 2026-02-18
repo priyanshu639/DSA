05c2ec4b84223e64d6e7fdb9c26ad2b373f6b656
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-02-18 14:10:12 +0530

    Time: 14 ms (43.20%) | Memory: 48.7 MB (93.67%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,63 +1,26 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int node;
-        int wt;
-        
-        Pair(int node,int wt){
-            this.node=node;
-            this.wt=wt;
-            
-        }
-        public int compareTo(Pair other){
-            return this.wt-other.wt;
-        }
-    }
+    // Bellmans Algortithm
     public int networkDelayTime(int[][] times, int n, int k) {
-        // create adj list
-        List<List<Pair>>adj=new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<times.length;i++){
-            int u=times[i][0];
-            int v=times[i][1];
-            int w=times[i][2];
-            adj.get(u).add(new Pair(v,w));
-            
-        }
-        int dist[]=new int[n+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
-        PriorityQueue<Pair>pq=new PriorityQueue<>();
-        pq.add(new Pair(k,0));
-        
-
-        
-        while(pq.size()>0){
-            Pair top=pq.remove();
-            int node=top.node;
-            int wt=top.wt;
-            
-            for(Pair ele:adj.get(node)){
-                int totalwt=wt+ele.wt;
-                if(wt > dist[node]) continue;
-
-                if(totalwt<dist[ele.node]){
-                    dist[ele.node]=totalwt;
-                    pq.add(new Pair(ele.node,totalwt));
+        int dis[]=new int[n+1];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[k]=0;
+        for(int x=0;x<=n-1;x++){
+            for(int i=0;i<times.length;i++){
+                int u=times[i][0];
+                int v=times[i][1];
+                int wt=times[i][2];
+                if(dis[u]!=Integer.MAX_VALUE && dis[u]+wt<dis[v]){
+                    dis[v]=dis[u]+wt;
                 }
+
             }
 
+        }int max=0;
+        for(int i=1;i<=n;i++){
+            if(dis[i]==Integer.MAX_VALUE)return -1;
+            max=Math.max(max,dis[i]);
         }
-        int max=0;
-        for(int i=1;i<dist.length;i++){
-            max=Math.max(dist[i],max);
-
-        }
-        if(max==Integer.MAX_VALUE) return -1;
         return max;
-
-
         
     }
 }
