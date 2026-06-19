b8909d218161ef371f0236b0aa2a394328edb2f7
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-06-20 04:59:55 +0530

    Time: 2 ms (94.65%) | Memory: 64.9 MB (80.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // optimal approach
-        int prefix[]=new int[nums.length];
-        int suffix[]=new int[nums.length];
-        int ans[]=new int[nums.length];
+        int n=nums.length;
+        int mul=1;
+        int ans[]=new int[n];
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
         prefix[0]=1;
-        suffix[nums.length-1]=1;
-        // prefix
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
-        }
-        // suffix
-         for(int i=nums.length-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i+1];
-            
-        }
-        // product of suffix and prefix array
-        // we can do it without creating ans array we can do changes in any of prefix or suffix array
+        suffix[n-1]=1;
 
-        // we can also do this without creating a suffix array but how?--> we can create by ttaking a variable suffix and store 1 and then every time we manipulate it by suffix*=nums[i+1]; and prefix[i]*=suffix;
-        for(int i=0;i<nums.length;i++){
-            ans[i]=prefix[i]*suffix[i];
+        for(int i=0;i<n-1;i++){
+            prefix[i+1]=prefix[i]*nums[i];
+
+
         }
-        
+        for(int i=n-1;i>0;i--){
+            suffix[i-1]=nums[i]*suffix[i];
+
+        }
+        for(int i=0;i<n;i++){
+            ans[i]=suffix[i]*prefix[i];
+        }
         return ans;
-
-
-
         
     }
 }
