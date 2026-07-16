4caa771284bd8ee0797101789cc2961185143d88
Priyanshu Prajapati <154823006+priyanshu639@users.noreply.github.com>
2026-07-17 00:30:43 +0530

    Time: 1 ms (99.97%) | Memory: 46.9 MB (46.08%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,21 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
+        int ans=0;
         for(int i=0;i<nums.length;i++){
-            boolean flag=false;
-            for(int j=0;j<nums.length;j++){
-                if(i==j) continue;
-                if(nums[i]==nums[j]){
-                    
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false) return nums[i];
-
+            ans=ans^nums[i];
         }
-        
-        return 23;
+        return ans;
         
     }
 }
